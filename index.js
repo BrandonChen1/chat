a4087ba9c0f68477e88ebd33b453392f4fe207d6
Brandon <bxc4081@rit.edu>
2019-03-07 19:48:57 -0500

    added private messaging

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')(),
     http = require('http').Server(app),
     io = require('socket.io')(http),
-    nicknames = [];
+    users = {};
 
 app.get('/', function(req, res){
  res.sendFile(__dirname + '/chat.html');
@@ -10,28 +10,49 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
     socket.on('new user', function(data, callback){
-        if (nicknames.indexOf(data) != -1){
+        if (data in users){
             callback(false);
         }
         else{
             callback(true);
             socket.nickname = data;
-            nicknames.push(socket.nickname);
+            users[socket.nickname] = socket;
             updateNicknames();
         }
     });
-    socket.on('send message', function(data){
-        io.emit('chat message', {msg: data, nick: socket.nickname});
+    socket.on('send message', function(data, callback){
+        var msg = data.trim();
+        if(msg.substr(0,3) === '/w '){
+            msg = msg.substr(3);
+            var ind = msg.indexOf(' ');
+            if(ind !== -1){
+                var name = msg.substring(0, ind);
+                var msg = msg.substring(ind + 1);
+                if (name in users){
+                    users[name].emit('private message', {msg: msg, nick: socket.nickname})
+                    console.log('whisper');
+                }
+                else{
+                    callback('Error! Enter a valid user');
+                }
+            }
+            else{
+                callback('Error: Please enter a message for your whipser');
+            }
+        }
+        else{
+            io.emit('chat message', {msg: msg, nick: socket.nickname});
+        }
     });
 
     function updateNicknames(){
-        io.emit('usernames', nicknames);
+        io.emit('usernames', Object.keys(users));
     }
 
     socket.on('disconnect', function(data){
        if(!socket.nickname) return;
        io.emit('logout', socket.nickname);
-       nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+       delete users[socket.nickname];
        updateNicknames();
     });
 });
